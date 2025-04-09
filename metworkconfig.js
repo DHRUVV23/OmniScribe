0e9a98758b33d41f494bc8ff27f8583f1da41efe
Dhruv23 <Agrawaldhruv233>
2025-04-09 23:43:57 +0530

    removed_test_files

--- metworkconfig.js
+++
@@ -1,8 +0,0 @@
-import { HttpsProxyAgent } from 'https-proxy-agent';
-
-export const networkSettings = {
-  proxy: process.env.HTTP_PROXY 
-    ? new HttpsProxyAgent(process.env.HTTP_PROXY)
-    : null,
-  timeout: 60000
-};
